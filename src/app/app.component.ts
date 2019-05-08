82153c3adcbd604c75bfd2d92deeab2d8adc39f7
Dilip Kumar <dilkumar@google.com>
2019-05-08 16:12:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  constructor( private http: HttpClient ){ };
+
+  
+  title = 'Hello World';
+
+  todos = this.http.get('https://jsonplaceholder.typicode.com/todos/');
 }
